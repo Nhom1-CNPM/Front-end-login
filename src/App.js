8e4763da880d7dc1385e259051f97b83da9e9f9e
ThienTrieu04 <toilatrieu2004@gmail.com>
2025-04-22 16:39:32 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
 import React from 'react';
+import { AuthProvider } from './AuthContext'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
+import ForgotPasswordForm from './components/ForgotPasswordForm';
+import ResetPasswordForm from './components/ResetPasswordForm';
 
 
 function App() {
     return (
-        <div>
-            <LoginForm />
-        </div>
+        <AuthProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<LoginForm />} />
+                    <Route path="/login" element={<LoginForm />} />
+                    <Route path="/forgot-password" element={<ForgotPasswordForm />} />
+                    <Route path="/reset-password" element={<ResetPasswordForm />} />
+                </Routes>
+            </Router>
+        </AuthProvider>
     );
 }
 
